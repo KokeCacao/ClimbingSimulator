325e6eb56ef18c59b1169cdc15c32b80c4124da7
Hanke Chen <i@kokecacao.me>
2023-04-19 20:11:50 -0400

    :sparkles: port menu to arm scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+  GameObject menu;
+  GameObject minimap;
 
-    public void QuitGame()
-    {
-        Debug.Log ("Quit");
-        Application.Quit();
-    }
+  public void Awake()
+  {
+    menu = GameObject.Find("Menu");
+    minimap = GameObject.Find("MinimapObject");
+    
+    menu.SetActive(true);
+    minimap.SetActive(false);
+  }
+
+  public void PlayGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void DisableMenu()
+  {
+    menu.SetActive(false);
+    minimap.SetActive(true);
+  }
+
+  public void QuitGame()
+  {
+    Debug.Log("Quit");
+    Application.Quit();
+  }
 }
